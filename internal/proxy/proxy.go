4481d68cd3a45dfc7f9ab5c12f6c09f4a393cde2
admirerr <shubhamkashyap0608@gmail.com>
2026-06-25 13:41:19 +0530

    feat(proxy): implement reverse proxy core

---
+++ internal/proxy/proxy.go
@@ -0,0 +1,45 @@
+package proxy
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+type ReverseProxy struct {
+	proxy *httputil.ReverseProxy
+}
+
+func New(target string) (*ReverseProxy, error) {
+	targetURL, err := url.Parse(target)
+	if err != nil {
+		return nil, err
+	}
+
+	rp := &httputil.ReverseProxy{
+		Rewrite: func(pr *httputil.ProxyRequest) {
+			pr.SetURL(targetURL)
+
+			// Remove any spoofed forwarding headers.
+			pr.Out.Header.Del("X-Forwarded-For")
+			pr.Out.Header.Del("X-Forwarded-Host")
+			pr.Out.Header.Del("X-Forwarded-Proto")
+
+			pr.SetXForwarded()
+
+			pr.Out.Header.Set("X-Aegora", "true")
+		},
+		Transport: NewTransport(),
+	}
+
+	return &ReverseProxy{
+		proxy: rp,
+	}, nil
+}
+
+func (r *ReverseProxy) ServeHTTP(
+	w http.ResponseWriter,
+	req *http.Request,
+) {
+	r.proxy.ServeHTTP(w, req)
+}
