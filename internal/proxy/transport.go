fc07ddaa7f677b95048410b0a46e3bec775dfa98
admirerr <shubhamkashyap0608@gmail.com>
2026-06-25 13:33:01 +0530

    feat(proxy): add custom HTTP transport

---
+++ internal/proxy/transport.go
@@ -0,0 +1,21 @@
+package proxy
+
+import (
+	"net"
+	"net/http"
+	"time"
+)
+
+func NewTransport() *http.Transport {
+	return &http.Transport{
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   50,
+		IdleConnTimeout:       90 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		DialContext: (&net.Dialer{
+			Timeout:   5 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+	}
+}
